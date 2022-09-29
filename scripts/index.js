3462781d98e02689e9244063dbbe9e34ead151fa
7Romain <osiiriis@ymail.com>
2022-09-29 10:03:48 +0200

    commit final

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,23 @@
-import { acc } from "./accordeon";
+const acc = function () {
+    const accordeon = document.getElementsByClassName("accordion");
+    for (let i = 0; i < accordeon.length; i++) {
+        accordeon[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            const panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+};
+
+acc();
 
 const section = document.getElementById("section");
 const code = document.getElementById("rechercher");
