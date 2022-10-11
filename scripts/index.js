b6ca5a7165df74d9d4dc6373c7b370509be613e0
7Romain <osiiriis@ymail.com>
2022-10-11 08:23:25 +0200

    la partie principale s'efface quand on lance la recherche.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable dot-notation */
 import { genererAccordeon } from "./accordeon.js";
 import { afficherImageProduit, afficherScores } from "./images.js";
 import { afficherTableau } from "./tableau.js";
@@ -27,28 +26,69 @@ const afficher = function (resultat) {
 };
 
 /**
+ * liste des champs à récuperer via l'api
+ */
+// const listeFields = [
+//     "&fields=",
+//     "product_name_fr",
+//     "product_name_en",
+//     "brands_imported",
+//     "brands",
+//     "quantity",
+//     "packaging_text_fr",
+//     "categories_old",
+//     "preparation_fr",
+//     "traces_imported",
+//     "stores",
+//     "conservation_conditions_fr",
+//     "ingredients_text_with_allergens_fr",
+//     "Ingredients_text_en",
+//     "image_small_url",
+//     "nutriscore_grade",
+//     "ecoscore_grade",
+//     "nova_group",
+//     "nutrition_data_prepared_per",
+//     "fat_100g",
+//     "energy-kj",
+//     "saturated-fat_100g",
+//     "carbohydrates_100g",
+//     "sugars_100g",
+//     "fiber_modifier",
+//     "fiber_100g",
+//     "proteins_100g",
+//     "salt_100g",
+//     "alcohol",
+//     "fruits-vegetables-nuts-estimate-from-ingredients_100g",
+// ];
+
+/**
  * Ajoute un écouteur sur le bouton rechercher qui lance la recherche et
  * rend visible la fiche produit.
  */
 
 formulaire.addEventListener("submit", function (e) {
+    section.classList.add("cacher");
     if (regex.test(code.value)) {
         e.preventDefault();
 
-        fetch("http://fr.openfoodfacts.org/api/v2/search?code=" + code.value)
+        fetch(
+            "http://fr.openfoodfacts.org/api/v2/search?code=" + code.value
+            // +listeFields.toString()
+        )
             .then((response) => response.json())
 
             .then(function (data) {
                 resultat = data;
-                console.table(resultat);
-                section.classList.add("sectionVisible");
-                afficher(resultat);
+                console.log(resultat);
 
                 if (resultat.count === 0) {
                     alert(
                         "Le produit n'est pas présent dans la base de données"
                     );
                 } else {
+                    section.classList.remove("cacher");
+                    section.classList.add("sectionVisible");
+                    afficher(resultat);
                     console.log("ok");
                 }
             })
